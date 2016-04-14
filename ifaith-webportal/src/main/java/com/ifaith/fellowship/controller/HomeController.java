d39e310e8fbbd68f88d75b2c9f55a16c781fc369
Alan luo <iteamalan@hotmail.com>
2016-04-14 17:41:12 +0800

    create base porject.

---
+++ ifaith-webportal/src/main/java/com/ifaith/fellowship/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.ifaith.fellowship.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller(value="/home")
+public class HomeController {
+	@RequestMapping(method=RequestMethod.GET)
+	public String showHomePage(Model model){
+		model.addAttribute("message", "This is test message!");
+		return "HomeView";
+	}
+}
